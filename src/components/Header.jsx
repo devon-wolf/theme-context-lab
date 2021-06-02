e6c618d1f0b7c30ed08b57a21f5dc099b1ce0a0a
Devon Wolfkiel <devonwolf@tutanota.com>
2021-06-02 10:06:45 -0700

    Build out basic header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const Header = () => {
+const Header = ({ handleThemeToggle }) => {
   return (
     <header>
-			
+      <h1>Theme Switch Lab</h1>
+      <button onClick={handleThemeToggle}>Toggle theme?</button>
     </header>
   );
+};
+
+Header.propTypes = {
+  handleThemeToggle: PropTypes.func.isRequired
 };
 
 export default Header;
