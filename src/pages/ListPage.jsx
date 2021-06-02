c8ad0f971ad04316d03a2684f6aae350215687d6
Devon Wolfkiel <devonwolf@tutanota.com>
2021-06-02 10:04:14 -0700

    Build out basic list page

--- src/pages/ListPage.jsx
+++ src/pages/ListPage.jsx
@@ -1,11 +1,23 @@
+/* eslint-disable max-len */
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const ListPage = () => {
+const ListPage = ({ results }) => {
   return (
     <main>
-			
+      <ul>
+        {results.map(result => (
+          <li key={result.name}>
+            {result.name}
+          </li>
+        ))}
+      </ul>
     </main>
   );
+};
+
+ListPage.propTypes = {
+  results: PropTypes.arrayOf(PropTypes.shape({ name: PropTypes.string.isRequired })).isRequired
 };
 
 export default ListPage;
